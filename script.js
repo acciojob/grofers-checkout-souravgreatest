f08897ca9c76a64c3cb628c53ac0b35206c36401
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-06 16:56:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,12 +11,14 @@ const getSum = () => {
 	})
 	const table=document.querySelector('table')
 	const new_row=document.createElement('tr')
+	new_row.setAttribute("id","ans");
 	// const total_sum=document.createElement('td')
 	// total_sum.textContent='Total Price';
 	// new_row.appendChild(total_sum);
 	const total_price=document.createElement('td')
+	total_price.setAttribute("id","ans");
 	total_price.setAttribute("colspan","2");
-	total_price.style.cssText=" text-align:center";
+	// total_price.style.cssText=" text-align:center";
 	total_price.textContent=sum;
 	new_row.appendChild(total_price)
 	table.appendChild(new_row);  
